771b98938a69879ee24d04002f219852419dcfc7
Sumir Vats <sumirvats@gmail.com>
2023-02-16 21:47:02 +0530

    Added User route, controller and model

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 3000;
+dotenv.config();
+
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.patk37o.mongodb.net/Blog?retryWrites=true&w=majority`;
+
+app.use("/api/user", router);
+mongoose
+  .connect(connectionString)
+  .then(() => app.listen(PORT))
+  .then(() => console.log(`App listening on port ${PORT}`))
+  .catch((err) => console.log(err));
